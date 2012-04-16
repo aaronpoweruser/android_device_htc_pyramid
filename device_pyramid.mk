5c6896f1468d234c81211c656ae24d872564e405
aaronpoweruser <aaaronpoweruser@yahoo.com>
2012-04-15 22:53:57 -0700

    shooterfied

--- device_pyramid.mk
+++ device_pyramid.mk
@@ -24,8 +24,8 @@ DEVICE_PACKAGE_OVERLAYS += device/htc/pyramid/overlay
 
 # GPS and Light
 PRODUCT_PACKAGES += \
-    gps.pyramid \
-    lights.pyramid
+    gps.shooter \
+    lights.shooter
 
 # Torch
 PRODUCT_PACKAGES += \
@@ -43,65 +43,65 @@ $(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4329
 
 # Boot ramdisk setup
 PRODUCT_COPY_FILES += \
-    device/htc/pyramid/prebuilt/init:root/init \
-    device/htc/pyramid/ramdisk/init.pyramid.rc:root/init.pyramid.rc \
-    device/htc/pyramid/ramdisk/init.pyramid.usb.rc:root/init.pyramid.usb.rc \
-    device/htc/pyramid/ramdisk/ueventd.pyramid.rc:root/ueventd.pyramid.rc
+    device/htc/shooter/prebuilt/init:root/init \
+    device/htc/shooter/ramdisk/init.shooter.rc:root/init.shooter.rc \
+    device/htc/shooter/ramdisk/init.shooter.usb.rc:root/init.shooter.usb.rc \
+    device/htc/shooter/ramdisk/ueventd.shooter.rc:root/ueventd.shooter.rc
 
 ## recovery and custom charging
 PRODUCT_COPY_FILES += \
-    device/htc/pyramid/prebuilt/init:recovery/root/init \
-    device/htc/pyramid/recovery/sbin/choice_fn:recovery/root/sbin/choice_fn \
-    device/htc/pyramid/recovery/sbin/power_test:recovery/root/sbin/power_test \
-    device/htc/pyramid/recovery/sbin/offmode_charging:recovery/root/sbin/offmode_charging \
-    device/htc/pyramid/recovery/sbin/detect_key:recovery/root/sbin/detect_key \
-    device/htc/pyramid/recovery/sbin/htcbatt:recovery/root/sbin/htcbatt
+    device/htc/shooter/prebuilt/init:recovery/root/init \
+    device/htc/shooter/recovery/sbin/choice_fn:recovery/root/sbin/choice_fn \
+    device/htc/shooter/recovery/sbin/power_test:recovery/root/sbin/power_test \
+    device/htc/shooter/recovery/sbin/offmode_charging:recovery/root/sbin/offmode_charging \
+    device/htc/shooter/recovery/sbin/detect_key:recovery/root/sbin/detect_key \
+    device/htc/shooter/recovery/sbin/htcbatt:recovery/root/sbin/htcbatt
 
 # Some misc configeration files
 PRODUCT_COPY_FILES += \
-    device/htc/pyramid/vold.fstab:system/etc/vold.fstab
+    device/htc/shooter/vold.fstab:system/etc/vold.fstab
 
 # Keylayouts and Keychars
 PRODUCT_COPY_FILES += \
-    device/htc/pyramid/keychars/pyramid-keypad.kcm:system/usr/keychars/pyramid-keypad.kcm \
-    device/htc/pyramid/keylayout/AVRCP.kl:system/usr/keylayout/AVRCP.kl \
-    device/htc/pyramid/keylayout/cy8c-touchscreen.kl:system/usr/keylayout/cy8c-touchscreen.kl \
-    device/htc/pyramid/keylayout/h2w_headset.kl:system/usr/keylayout/h2w_headset.kl \
-    device/htc/pyramid/keylayout/pyramid-keypad.kl:system/usr/keylayout/pyramid-keypad.kl
+    device/htc/shooter/keychars/pyramid-keypad.kcm:system/usr/keychars/pyramid-keypad.kcm \
+    device/htc/shooter/keylayout/AVRCP.kl:system/usr/keylayout/AVRCP.kl \
+    device/htc/shooter/keylayout/cy8c-touchscreen.kl:system/usr/keylayout/cy8c-touchscreen.kl \
+    device/htc/shooter/keylayout/h2w_headset.kl:system/usr/keylayout/h2w_headset.kl \
+    device/htc/shooter/keylayout/pyramid-keypad.kl:system/usr/keylayout/pyramid-keypad.kl
 
 # Input device config
 PRODUCT_COPY_FILES += \
-    device/htc/pyramid/idc/cy8c-touchscreen.idc:system/usr/idc/cy8c-touchscreen.idc \
-    device/htc/pyramid/idc/synaptics-rmi-touchscreen.idc:system/usr/idc/synaptics-rmi-touchscreen.idc
+    device/htc/shooter/idc/cy8c-touchscreen.idc:system/usr/idc/cy8c-touchscreen.idc \
+    device/htc/shooter/idc/synaptics-rmi-touchscreen.idc:system/usr/idc/synaptics-rmi-touchscreen.idc
 
 # HTC BT Audio tune
 PRODUCT_COPY_FILES += device/htc/pyramid/configs/AudioBTID.csv:system/etc/AudioBTID.csv
 
 # Sound configs
 PRODUCT_COPY_FILES += \
-    device/htc/pyramid/dsp/AdieHWCodec.csv:system/etc/AdieHWCodec.csv \
-    device/htc/pyramid/dsp/AIC3254_REG.csv:system/etc/AIC3254_REG.csv \
-    device/htc/pyramid/dsp/AIC3254_REG_DualMic.csv:system/etc/AIC3254_REG_DualMic.csv \
-    device/htc/pyramid/dsp/CodecDSPID.txt:system/etc/CodecDSPID.txt \
-    device/htc/pyramid/dsp/CodecDSPID_WB.txt:system/etc/CodecDSPID_WB.txt \
-    device/htc/pyramid/dsp/TPA2051_CFG.csv:system/etc/TPA2051_CFG.csv \
-    device/htc/pyramid/dsp/TPA2051_CFG_XC.csv:system/etc/TPA2051_CFG_XC.csv \
-    device/htc/pyramid/dsp/soundimage/Sound_MFG.txt:system/etc/soundimage/Sound_MFG.txt \
-    device/htc/pyramid/dsp/soundimage/Sound_Original_Recording.txt:system/etc/soundimage/Sound_Original_Recording.txt \
-    device/htc/pyramid/dsp/soundimage/Sound_Original_SPK.txt:system/etc/soundimage/Sound_Original_SPK.txt \
-    device/htc/pyramid/dsp/soundimage/Sound_Original.txt:system/etc/soundimage/Sound_Original.txt \
-    device/htc/pyramid/dsp/soundimage/Sound_Phone_Original_HP_LE.txt:system/etc/soundimage/Sound_Phone_Original_HP_LE.txt \
-    device/htc/pyramid/dsp/soundimage/Sound_Phone_Original_HP.txt:system/etc/soundimage/Sound_Phone_Original_HP.txt \
-    device/htc/pyramid/dsp/soundimage/Sound_Phone_Original_HP_WB_LE.txt:system/etc/soundimage/Sound_Phone_Original_HP_WB_LE.txt \
-    device/htc/pyramid/dsp/soundimage/Sound_Phone_Original_HP_WB.txt:system/etc/soundimage/Sound_Phone_Original_HP_WB.txt \
-    device/htc/pyramid/dsp/soundimage/Sound_Phone_Original_REC_NEL.txt:system/etc/soundimage/Sound_Phone_Original_REC_NEL.txt \
-    device/htc/pyramid/dsp/soundimage/Sound_Phone_Original_REC.txt:system/etc/soundimage/Sound_Phone_Original_REC.txt \
-    device/htc/pyramid/dsp/soundimage/Sound_Phone_Original_REC_WB.txt:system/etc/soundimage/Sound_Phone_Original_REC_WB.txt \
-    device/htc/pyramid/dsp/soundimage/Sound_Phone_Original_SPK.txt:system/etc/soundimage/Sound_Phone_Original_SPK.txt \
-    device/htc/pyramid/dsp/soundimage/Sound_Phone_Original_SPK_WB.txt:system/etc/soundimage/Sound_Phone_Original_SPK_WB.txt \
-    device/htc/pyramid/dsp/soundimage/Sound_Rec_Landscape.txt:system/etc/soundimage/Sound_Rec_Landscape.txt \
-    device/htc/pyramid/dsp/soundimage/Sound_Recording.txt:system/etc/soundimage/Sound_Recording.txt \
-    device/htc/pyramid/dsp/soundimage/Sound_Rec_Portrait.txt:system/etc/soundimage/Sound_Rec_Portrait.txt
+    device/htc/shooter/dsp/AdieHWCodec.csv:system/etc/AdieHWCodec.csv \
+    device/htc/shooter/dsp/AIC3254_REG.csv:system/etc/AIC3254_REG.csv \
+    device/htc/shooter/dsp/AIC3254_REG_DualMic.csv:system/etc/AIC3254_REG_DualMic.csv \
+    device/htc/shooter/dsp/CodecDSPID.txt:system/etc/CodecDSPID.txt \
+    device/htc/shooter/dsp/CodecDSPID_WB.txt:system/etc/CodecDSPID_WB.txt \
+    device/htc/shooter/dsp/TPA2051_CFG.csv:system/etc/TPA2051_CFG.csv \
+    device/htc/shooter/dsp/TPA2051_CFG_XC.csv:system/etc/TPA2051_CFG_XC.csv \
+    device/htc/shooter/dsp/soundimage/Sound_MFG.txt:system/etc/soundimage/Sound_MFG.txt \
+    device/htc/shooter/dsp/soundimage/Sound_Original_Recording.txt:system/etc/soundimage/Sound_Original_Recording.txt \
+    device/htc/shooter/dsp/soundimage/Sound_Original_SPK.txt:system/etc/soundimage/Sound_Original_SPK.txt \
+    device/htc/shooter/dsp/soundimage/Sound_Original.txt:system/etc/soundimage/Sound_Original.txt \
+    device/htc/shooter/dsp/soundimage/Sound_Phone_Original_HP_LE.txt:system/etc/soundimage/Sound_Phone_Original_HP_LE.txt \
+    device/htc/shooter/dsp/soundimage/Sound_Phone_Original_HP.txt:system/etc/soundimage/Sound_Phone_Original_HP.txt \
+    device/htc/shooter/dsp/soundimage/Sound_Phone_Original_HP_WB_LE.txt:system/etc/soundimage/Sound_Phone_Original_HP_WB_LE.txt \
+    device/htc/shooter/dsp/soundimage/Sound_Phone_Original_HP_WB.txt:system/etc/soundimage/Sound_Phone_Original_HP_WB.txt \
+    device/htc/shooter/dsp/soundimage/Sound_Phone_Original_REC_NEL.txt:system/etc/soundimage/Sound_Phone_Original_REC_NEL.txt \
+    device/htc/shooter/dsp/soundimage/Sound_Phone_Original_REC.txt:system/etc/soundimage/Sound_Phone_Original_REC.txt \
+    device/htc/shooter/dsp/soundimage/Sound_Phone_Original_REC_WB.txt:system/etc/soundimage/Sound_Phone_Original_REC_WB.txt \
+    device/htc/shooter/dsp/soundimage/Sound_Phone_Original_SPK.txt:system/etc/soundimage/Sound_Phone_Original_SPK.txt \
+    device/htc/shooter/dsp/soundimage/Sound_Phone_Original_SPK_WB.txt:system/etc/soundimage/Sound_Phone_Original_SPK_WB.txt \
+    device/htc/shooter/dsp/soundimage/Sound_Rec_Landscape.txt:system/etc/soundimage/Sound_Rec_Landscape.txt \
+    device/htc/shooter/dsp/soundimage/Sound_Recording.txt:system/etc/soundimage/Sound_Recording.txt \
+    device/htc/shooter/dsp/soundimage/Sound_Rec_Portrait.txt:system/etc/soundimage/Sound_Rec_Portrait.txt
 
 # Permissions
 PRODUCT_COPY_FILES += \
@@ -116,18 +116,18 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dexopt-flags=m=y
 
 ifeq ($(TARGET_PREBUILT_KERNEL),)
-LOCAL_KERNEL := device/htc/pyramid/prebuilt/kernel
+LOCAL_KERNEL := device/htc/shooter/prebuilt/kernel
 else
 LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel \
-    device/htc/pyramid/modules/bcmdhd.ko:/system/lib/modules/bcmdhd.ko \
-    device/htc/pyramid/modules/scsi_wait_scan.ko:/system/lib/modules/scsi_wait_scan.ko
+    device/htc/shooter/modules/bcmdhd.ko:/system/lib/modules/bcmdhd.ko \
+    device/htc/shooter/modules/scsi_wait_scan.ko:/system/lib/modules/scsi_wait_scan.ko
 
 # call the proprietary setup
-$(call inherit-product-if-exists, vendor/htc/pyramid/pyramid-vendor.mk)
+$(call inherit-product-if-exists, vendor/htc/shooter/shooter-vendor.mk)
 
 # call the gapps setup
 $(call inherit-product-if-exists, vendor/google/gapps.mk)
